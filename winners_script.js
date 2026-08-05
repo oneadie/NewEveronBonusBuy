0f43bf49eb4b22113f6282f870d96ebed7214e7b
oneadie <mega585588@gmail.com>
2026-08-05 13:58:21 +0300

    Refactor winners_script.js for clarity and structure

--- winners_script.js
+++ winners_script.js
@@ -265,7 +265,7 @@ function generateOBSLink(currency) {
     if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
         baseUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
     } else {
-        baseUrl = 'https://oneadie.github.io/EveronBonusBuy/winners_widget.html';
+        baseUrl = 'https://oneadie.github.io/NewEveronBonusBuy/winners_widget.html';
     }
     const url = `${baseUrl}?obs=1&currency=${currency}&_=${Date.now()}`;
     const input = document.getElementById('obs-url');
